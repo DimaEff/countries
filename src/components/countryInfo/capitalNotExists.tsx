169e1de90d59faebfe747cefa66936f648756095
Dima Fominenkov <69797190+DimaEff@users.noreply.github.com>
2022-08-02 04:27:42 +0300

    added capitalNotExists

---
+++ src/components/countryInfo/capitalNotExists.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Typography } from "@mui/material";
+
+const CapitalNotExists = () => {
+    return (
+        <Typography variant={"h6"} textAlign={"center"} color={"#4f4f4f"} fontWeight={"bold"} margin={3}>
+            Capital not exists
+        </Typography>
+    );
+};
+
+export default CapitalNotExists;
--- src/components/countryInfo/index.tsx
+++ src/components/countryInfo/index.tsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 
 import { fetchCountryByCode } from "@components/countryInfo/api";
 import CapitalMap from "@components/countryInfo/capitalMap";
+import CapitalNotExists from "@components/countryInfo/capitalNotExists";
 import { FETCH_COUNTRY_BY_CODE } from "@components/countryInfo/consts";
 import MainInfo from "@components/countryInfo/mainInfo";
 import FullscreenLoader from "@shared/components/fullscreenLoader";
@@ -20,7 +21,7 @@ const CountryInfo = () => {
     return (
         <div>
             {data && <MainInfo country={data} />}
-            {data?.capital && <CapitalMap capital={data.capital} />}
+            {data?.capital ? <CapitalMap capital={data.capital} /> : <CapitalNotExists />}
         </div>
     );
 };
